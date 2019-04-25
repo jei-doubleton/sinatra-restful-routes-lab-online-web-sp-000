6a18f6fca8ecacbacf245226b800e553ade20e89
J. Singleton <jennifer.singleton@gmail.com>
2019-04-25 02:45:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
     @recipe.cook_time = params[:cook_time]
     @recipe.save
 
-    redirect '/recipes/:id'
+    redirect "/recipes/#{@recipe.id}"
   end
 
 end
